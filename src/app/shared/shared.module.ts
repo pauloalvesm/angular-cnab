a6de1c84ab45f1a754562e5556d37f32b8269749
pauloalvesm <paulo.alves7351@gmail.com>
2026-04-01 19:59:42 -0300

    feat: add header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule { }
