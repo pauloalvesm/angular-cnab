52395524d87fe19c9354dd07e6e3d69185f055bd
pauloalvesm <paulo.alves7351@gmail.com>
2026-04-01 20:01:09 -0300

    feat: apply routes to modules

--- src/app/modules/cnab/cnab.module.ts
+++ src/app/modules/cnab/cnab.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CnabFileComponent } from './components/cnab-file/cnab-file.component';
+import { CNAB_ROUTES } from './routes/cnab.routing';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     CnabFileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(CNAB_ROUTES),
   ],
   exports: [
     CnabFileComponent
--- src/app/modules/store/store.module.ts
+++ src/app/modules/store/store.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreListComponent } from './components/store-list/store-list.component';
+import { STORE_ROUTES } from './routes/store.routing';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     StoreListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(STORE_ROUTES)
   ],
   exports: [
     StoreListComponent
