7b60f39d99f5e8c5458cde6f9f83248fcb869e51
pauloalvesm <paulo.alves7351@gmail.com>
2026-04-12 12:33:07 -0300

    feat: create functions to format fields

--- src/app/shared/utils/utility.ts
+++ src/app/shared/utils/utility.ts
@@ -22,4 +22,24 @@ export class Utility {
       }
     }
   }
+
+  public formatCpf(value: string): string {
+    if (!value) return '';
+    const numbers = value.replace(/\D/g, '');
+    return numbers
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1');
+  }
+
+  public formatCard(value: string): string {
+    if (!value) return '';
+
+    const cleanValue = value.replace(/\s/g, '');
+
+    return cleanValue
+      .replace(/(.{4})(?=.+)/g, '$1 ')
+      .substring(0, 19);
+  }
 }
