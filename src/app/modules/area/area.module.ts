c7dc5efd8dfdf2ac1deb9d22f2812767292467d8
pauloalvesm <paulo.alves7351@gmail.com>
2026-04-22 20:22:31 -0300

    feat: add dashboard component

--- src/app/modules/area/area.module.ts
+++ src/app/modules/area/area.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AREA_ROUTES } from './routes/area.routing';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(AREA_ROUTES)
   ],
   exports: [
     DashboardComponent
