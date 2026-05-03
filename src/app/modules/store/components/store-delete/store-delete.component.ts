1a21c60470edcbd4f65e463ac329206863249bd5
pauloalvesm <paulo.alves7351@gmail.com>
2026-05-03 11:34:56 -0300

    feat: set up alerts to exclude stores

--- src/app/modules/store/components/store-delete/store-delete.component.ts
+++ src/app/modules/store/components/store-delete/store-delete.component.ts
@@ -2,11 +2,15 @@ import {
   Component,
   ElementRef,
   EventEmitter,
+  inject,
   Output,
   ViewChild,
 } from '@angular/core';
 import { Store } from '../../../../core/models/interfaces/store.model';
 import { StoreService } from '../../services/store/store.service';
+import { AlertService } from '../../../../shared/services/alert/alert.service';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-store-delete',
@@ -17,6 +21,8 @@ import { StoreService } from '../../services/store/store.service';
 export class StoreDeleteComponent {
   selectedStore: Store | null = null;
   isDeleting = false;
+
+  private alertService = inject(AlertService);
 
   @ViewChild('closeBtn') closeBtn!: ElementRef;
   @Output() storeDeleted = new EventEmitter<void>();
@@ -34,13 +40,31 @@ export class StoreDeleteComponent {
     this.storeService.deleteStore(this.selectedStore.id).subscribe({
       next: () => {
         this.isDeleting = false;
+        this.closeModal();
         this.storeDeleted.emit();
-        this.closeBtn.nativeElement.click();
+        this.alertService.showAlert('success', 'Store deleted successfully!');
       },
       error: (err) => {
         console.error('Error deleting store:', err);
         this.isDeleting = false;
+        this.closeModal();
+
+        this.alertService.showAlert(
+          'danger',
+          'Stores can only be deleted by the administrator user.'
+        );
       },
     });
   }
+
+  public closeModal(): void {
+    const modalElement = document.getElementById('storeDeleteModal');
+    if (modalElement) {
+      const modalInstance = bootstrap.Modal.getOrCreateInstance(modalElement);
+      modalInstance.hide();
+    } else {
+      this.closeBtn.nativeElement.click();
+    }
+  }
+
 }
